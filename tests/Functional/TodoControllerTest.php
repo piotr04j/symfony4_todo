fa521873e462d6ded4daaef8a62173a3689b04ea
Piotr <you@example.com>
2019-06-09 14:29:05 +0200

    added more functional tests

--- tests/Functional/TodoControllerTest.php
+++ tests/Functional/TodoControllerTest.php
@@ -7,7 +7,6 @@ namespace App\Tests\Functional;
 
 use App\DataFixtures\TaskFixtures;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Doctrine\Common\Persistence\ObjectManager;
 
 /**
  * @property \Symfony\Bundle\FrameworkBundle\Client client
@@ -19,6 +18,7 @@ class TodoControllerTest extends WebTestCase
     protected function setUp(): void
     {
         $this->client = static::createClient();
+
         $doctrine= $this->client ->getContainer()->get('doctrine');
         $entityManager = $doctrine->getManager();
 
@@ -37,43 +37,80 @@ class TodoControllerTest extends WebTestCase
     /**
      * @test
      */
-    public function formRenderingTest()
+    public function pageRenderedCorrectly()
     {
 
-        $crawler = $this->client->request('POST', '/');
+        $crawler = $this->client->request('GET', '/');
 
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
 
-//        $this->assertEquals(
-//            'To do app!',
-//            $crawler->filter('h1')->text()
-//        );
-//
         $this->assertEquals(
-            3,
-            $crawler->filter('li')->count()
+            'To do app!',
+            $crawler->filter('h1')->text()
         );
-//
-//
-//        $form = $crawler->selectButton('Submit')->form();
-//        $form['add_task[task]'] = 'sss';
-//
-//        $crawler = $this->client->submit($form);
-//        $crawler = $this->client->followRedirect();
-//
-//        $this->assertEquals(3, $crawler->filter('li')->count());
-//        $this->assertEquals(
-//            TaskFixtures::CONTENT_WIP,
-//            $crawler->filter('h1')->text()
-//        );
-//
-//        $this->assertEquals(
-//            TaskFixtures::CONTENT_DONE,
-//            $crawler->filter('h1')->text()
-//        );
+
+        $this->assertEquals(
+            1,
+            $crawler->filter('form')->count()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addedTaskProperly()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $form = $crawler
+            ->selectButton('Submit')
+            ->form([
+                'add_task[task]' => 'new task'
+            ]);
 
 
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
 
+        $this->assertEquals(4, $crawler->filter('li')->count());
+        $this->assertEquals(1, $crawler->filterXPath('//li[contains(text(),\'new task\')]')->count());
+
+    }
+
+    /**
+     * @test
+     */
+    public function deleteTasks()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $task = $crawler
+            ->filterXPath('//li[contains(text(),\''. TaskFixtures::CONTENT_TODO .'\')]//a[contains(text(),\'DELETE\')]')
+            ->link();
+
+        $this->client->click($task);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(2, $crawler->filter('li')->count());
+        $this->assertEquals(0, $crawler->filterXPath('//li[contains(text(),\''. TaskFixtures::CONTENT_TODO  .'\')]')->count());
+
+        $task = $crawler
+            ->filterXPath('//li[contains(text(),\''. TaskFixtures::CONTENT_WIP .'\')]//a[contains(text(),\'DELETE\')]')
+            ->link();
+
+        $this->client->click($task);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(1, $crawler->filter('li')->count());
+        $this->assertEquals(0, $crawler->filterXPath('//li[contains(text(),\''. TaskFixtures::CONTENT_WIP  .'\')]')->count());
+
+
+        $task = $crawler
+            ->filterXPath('//li[contains(text(),\''. TaskFixtures::CONTENT_DONE .'\')]//a[contains(text(),\'DELETE\')]')
+            ->link();
+
+        $this->client->click($task);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(0, $crawler->filter('li')->count());
 
     }
 }
